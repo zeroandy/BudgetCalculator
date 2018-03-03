4483d181624f8ab448fbd31c6520caf4ed4b478c
hatelove <jay.hatelove@gmail.com>
2018-03-03 11:20:13 +0800

    refactor GetOneMonthAmount(), 1. extract method DailyAmount(), move into Budget class; 2. Move EffectiveDays() to Period class

--- BudgetCalculator/Accounting.cs
+++ BudgetCalculator/Accounting.cs
@@ -4,6 +4,32 @@ using System.Linq;
 
 namespace BudgetCalculator
 {
+    internal class Period
+    {
+        public Period(DateTime start, DateTime end)
+        {
+            if (start > end)
+            {
+                throw new ArgumentException();
+            }
+            Start = start;
+            End = end;
+        }
+
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public bool IsSameMonth()
+        {
+            return this.Start.Year == this.End.Year && this.Start.Month == this.End.Month;
+        }
+
+        public int EffectiveDays()
+        {
+            return (this.End - this.Start).Days + 1;
+        }
+    }
+
     internal class Accounting
     {
         private readonly IRepository<Budget> _repo;
@@ -15,14 +41,22 @@ namespace BudgetCalculator
 
         public decimal TotalAmount(DateTime start, DateTime end)
         {
-            if (start > end)
+            var period = new Period(start, end);
+
+            return period.IsSameMonth()
+                ? GetOneMonthAmount(period)
+                : GetRangeMonthAmount(start, end);
+        }
+
+        private int GetOneMonthAmount(Period period)
+        {
+            var budget = _repo.GetAll().Get(period.Start);
+            if (budget == null)
             {
-                throw new ArgumentException();
+                return 0;
             }
 
-            return IsSameMonth(start, end)
-                ? GetOneMonthAmount(start, end)
-                : GetRangeMonthAmount(start, end);
+            return budget.DailyAmount() * period.EffectiveDays();
         }
 
         private decimal GetRangeMonthAmount(DateTime start, DateTime end)
@@ -33,40 +67,19 @@ namespace BudgetCalculator
             {
                 if (index == 0)
                 {
-                    total += GetOneMonthAmount(start, start.LastDate());
+                    total += GetOneMonthAmount(new Period(start, start.LastDate()));
                 }
                 else if (index == monthCount)
                 {
-                    total += GetOneMonthAmount(end.FirstDate(), end);
+                    total += GetOneMonthAmount(new Period(end.FirstDate(), end));
                 }
                 else
                 {
                     var now = start.AddMonths(index);
-                    total += GetOneMonthAmount(now.FirstDate(), now.LastDate());
+                    total += GetOneMonthAmount(new Period(now.FirstDate(), now.LastDate()));
                 }
             }
             return total;
-        }
-
-        private bool IsSameMonth(DateTime start, DateTime end)
-        {
-            return start.Year == end.Year && start.Month == end.Month;
-        }
-
-        private int GetOneMonthAmount(DateTime start, DateTime end)
-        {
-            var list = this._repo.GetAll();
-            var budget = list.Get(start)?.Amount ?? 0;
-
-            var days = DateTime.DaysInMonth(start.Year, start.Month);
-            var validDays = GetValidDays(start, end);
-
-            return (budget / days) * validDays;
-        }
-
-        private int GetValidDays(DateTime start, DateTime end)
-        {
-            return (end - start).Days + 1;
         }
     }
 
@@ -94,6 +107,5 @@ namespace BudgetCalculator
         {
             return new DateTime(date.Year, date.Month, 1);
         }
-
     }
 }
